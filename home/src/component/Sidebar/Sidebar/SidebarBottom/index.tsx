8e7a0554a513ef9e9ef7696d8ab6cdfced598571
EnmmmmOvO <wangjinghan0218@gmail.com>
2024-02-01 13:27:53 +0800

    label checkbox with text

--- home/src/component/Sidebar/Sidebar/SidebarBottom/index.tsx
+++ home/src/component/Sidebar/Sidebar/SidebarBottom/index.tsx
@@ -1,21 +1,21 @@
-import React from 'react';
-import { Box, Divider, IconButton, useTheme } from '@mui/material';
+import React, { useContext } from 'react';
+import { Box, Divider, IconButton, Tooltip, useTheme } from '@mui/material';
 import GitHubIcon from '@mui/icons-material/GitHub';
+import { LanguageContext } from '../../../../context/LanguageContext';
 
 const SidebarBottom: React.FC = () => {
   const theme = useTheme();
+  const { content } = useContext(LanguageContext);
 
   return (
     <>
-      <Divider
-        sx={{
-          background: theme.colors.alpha.trueWhite[10]
-        }}
-      />
+      <Divider sx={{ background: theme.colors.alpha.trueWhite[10] }} />
       <Box sx={{ p: 2, pb: 0, width: '100%', display: 'flex', justifyContent: 'flex-end', alignItems: 'center' }}>
-        <IconButton aria-label="delete" color="inherit" href="https://github.com/enmmmmovo" target="_blank">
-          <GitHubIcon />
-        </IconButton>
+        <Tooltip title={content.jumpGithub} placeholder="top-start">
+          <IconButton aria-label="delete" color="inherit" href="https://github.com/enmmmmovo" target="_blank">
+            <GitHubIcon />
+          </IconButton>
+        </Tooltip>
       </Box>
     </>
   );
--- home/src/screen/aboutPage.tsx
+++ home/src/screen/aboutPage.tsx
@@ -1,15 +1,11 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import Copyright from '../component/Copyright';
 import { Box } from '@mui/material';
 
 const AboutPage: React.FC = () => {
-  useEffect(() => {
-
-  }, []);
-
-
   return (
     <Box height='calc(100vh - 80px)'>
+
       <Copyright />
     </Box>
   );
