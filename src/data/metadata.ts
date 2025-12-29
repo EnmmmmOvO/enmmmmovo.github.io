3c9338febb3e41d858d718bc6a8820b5a981256c
EnmmmmOvO <wangjinghan0218@gmail.com>
2025-12-30 02:54:45 +1100

    update ico and resume url

--- src/data/metadata.ts
+++ src/data/metadata.ts
@@ -1,8 +1,8 @@
 export const SERVER_URL: string = '';
 export const GITHUB_URL: string = 'https://github.com/enmmmmovo';
 export const LINKEDIN_URL: string = 'https://www.linkedin.com/in/wang-jinghan-722144309/';
-export const RESUME_URL_EN: string = 'https://enmmmmovo.github.io/resume/index_en.html';
-export const RESUME_URL_ZH: string = 'https://enmmmmovo.github.io/resume/index.html';
-export const NEW_WEBSITE_URL: string = 'https://willcode.io/';
+export const RESUME_URL_EN: string = 'https://drive.willcode.io/Jinghan-Wang-Resume.pdf';
+export const RESUME_URL_ZH: string = 'https://drive.willcode.io/Jinghan-Wang-Resume.pdf';
+export const NEW_WEBSITE_URL: string = 'https://www.willcode.io/';
 export const WECHAT_CODE: string = 'wjh20000218';
 export const EMAIL: string = 'william.wang@willcode.io'
