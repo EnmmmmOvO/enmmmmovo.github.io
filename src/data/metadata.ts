1a2d93ec3e5d1c0bc1576e04b595532c42f31365
Jinghan Wang <77719492+EnmmmmOvO@users.noreply.github.com>
2025-12-31 22:38:45 +1100

    Update LINKEDIN_URL to new profile link

--- src/data/metadata.ts
+++ src/data/metadata.ts
@@ -1,6 +1,6 @@
 export const SERVER_URL: string = '';
 export const GITHUB_URL: string = 'https://github.com/enmmmmovo';
-export const LINKEDIN_URL: string = 'https://www.linkedin.com/in/wang-jinghan-722144309/';
+export const LINKEDIN_URL: string = 'https://www.linkedin.com/in/jinghan-wang-willcode/';
 export const RESUME_URL_EN: string = 'https://drive.willcode.io/Jinghan-Wang-Resume.pdf';
 export const RESUME_URL_ZH: string = 'https://drive.willcode.io/Jinghan-Wang-Resume.pdf';
 export const NEW_WEBSITE_URL: string = 'https://www.willcode.io/';
